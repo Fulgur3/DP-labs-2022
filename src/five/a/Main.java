04e79afd0b248d6fe8d8fb2306221fb2891d7956
Fulgur3 <belokrsasha@gmail.com>
2022-10-14 21:43:42 +0300

    lab 5

---
+++ src/five/a/Main.java
@@ -0,0 +1,10 @@
+package five.a;
+
+public class Main {
+        public static void main(String[] args){
+            Recruits recruits = new Recruits(160);
+            recruits.printRecruits();
+
+            ThreadManager manager = new ThreadManager(recruits, 50);
+        }
+    }
