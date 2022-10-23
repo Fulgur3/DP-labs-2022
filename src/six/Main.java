401114d9134bbd3b091b1e6d26a411efb17898f8
Fulgur3 <belokrsasha@gmail.com>
2022-10-23 21:03:06 +0300

    lab6 done

---
+++ src/six/Main.java
@@ -0,0 +1,14 @@
+package six;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] sizes = {100, 1000};
+
+        for (int matrixSize : sizes) {
+            SimpleMatrix.calculate(args, matrixSize);
+            StringMatrix.calculate(args, matrixSize);
+            FoxMatrix.calculate(args, matrixSize);
+            CannonMatrix.calculate(args, matrixSize);
+        }
+    }
+}
